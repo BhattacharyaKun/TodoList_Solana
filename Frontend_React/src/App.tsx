a0064789e0f6b994dace9cead694415e60dfb360
BhattacharyaKun <bhattacharyakun@gmail.com>
2023-06-20 05:21:49 +0530

    Adding frontend and backend

--- Frontend_React/src/App.tsx
+++ Frontend_React/src/App.tsx
@@ -1,4 +1,3 @@
-import idl from "./assets/idl.json";
 import Body from './Body';
 import AddItem from './AddItem';
 import Footer from './Footer'
@@ -6,32 +5,14 @@ import Header from './Header'
 import React, { useEffect, useState } from 'react';
 import { ItemType } from './types';
 import SearchItem from './SearchItem';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
-import { Program, Idl, AnchorProvider, setProvider } from "@project-serum/anchor";
-import * as web3 from "@solana/web3.js";
-
-const MockWallet = {
-  publicKey: web3.Keypair.generate().publicKey,
-  signTransaction: () => Promise.reject(),
-  signAllTransactions: () => Promise.reject(),
-}
 
 function App() 
 {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem("list")!) || []);
   const [newItem, setNewItem] = useState<string>('');
   const [searchItem, setSearchItem] = useState<string>('');
-  const { connection } = useConnection();
-  const wallet = useAnchorWallet() || MockWallet;
-  // const provider = new AnchorProvider(connection, wallet, {});
-  // setProvider(provider);
-  const programId = new web3.PublicKey("FpbdhtfjWLGvRbLrxK2Q9aiyMoRxCkD2xd6qftbdk7Eh");
-  const program = new Program(idl as Idl, programId);
 
   useEffect(() => {
-    console.log(connection);
-    console.log(wallet);
     localStorage.setItem("list", JSON.stringify(items));
   }, [items]);
 
@@ -69,12 +50,6 @@ function App()
   }
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={[wallet]}>
-        <WalletModalProvider>
-            <WalletMultiButton />
-
-
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans"> 
       <div className='bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg'>
         <div className="mb-4">
@@ -92,9 +67,6 @@ function App()
         </div>
       </div>
     </div>
-    </WalletModalProvider>
-    </WalletProvider>
-</ConnectionProvider>
   )
 }
 
